afad65867aca8da5e6ebc98154939056c03a17b0
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-04-02 20:00:34 +0800

    excercise 2.11 done

---
+++ concatenate/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String str1 = "PHP Exercises and ";
+        String srt2 = "Python Exercises";
+        String result ;
+        System.out.println("String 1 : " + str1 +"\n" + "String 2 : " +  srt2);
+        result = str1.concat(srt2);
+        System.out.println("The concatenated string : " +"\n"+ result);
+    }
+}
